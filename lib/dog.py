973afb3f4339feceec3829f50a40efab297a144f
petermunene <peter.munene@student.moringaschool.com>
2025-05-12 20:44:15 +0300

    setter and getter

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,27 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    def __init__(self,name="doggo", breed="Mastiff"):
+        self.name=name
+        self.breed=breed
+    @property
+    def name  (self):
+        return self._name
+    @name.setter
+    def name (self,value):
+        if not isinstance(value, str) or not (1<len(value)<25):
+            print('Name must be string between 1 and 25 characters.')
+        else :
+            self._name=value
+
+
+    @property
+    def breed(self):
+        return self._breed 
+    @breed.setter
+    def breed (self,breed_name):
+        if breed_name in (APPROVED_BREEDS):
+            self._breed=breed_name
+        else :
+            print("Breed must be in list of approved breeds.")
     pass
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,26 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self, name="John",job="Admin"):
+        self.name=name
+        self.job=job
+    @property
+    def name (self):
+        return self._name
+    @name.setter
+    def name (self,value):
+        if not isinstance(value, str) or not (1<len(value)<25):
+            print("Name must be string between 1 and 25 characters.")
+        else :
+            new_name=value.title()
+            self._name=new_name
+    @property
+    def job(self):
+        return self._job
+    @job.setter
+    def job (self,job_name):
+        if job_name in (APPROVED_JOBS):
+            self._job=job_name
+        else :
+            print("Job must be in list of approved jobs.")
     pass
